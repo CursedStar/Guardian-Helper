dcee3444c20e59aa5e8da6f9f9c61643a2363659
Nickolas001 <85787978+Nickolas001@users.noreply.github.com>
2021-12-23 22:20:10 +0200

    Create reactionRoles.js

---
+++ Commands/Developer/reactionRoles.js
@@ -0,0 +1,5 @@
+const { CommandInteraction, MessageEmbed, MessageActionRow, MessageSelectMenu, MessageButton, MessageReaction, Collector} = require('discord.js');
+
+module.exports = {
+    name: ''
+}
