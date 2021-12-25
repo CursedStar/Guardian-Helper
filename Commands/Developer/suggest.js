bb8f8f4d8610e591689981bf8f0c437ae36927a0
Nickolas001 <85787978+Nickolas001@users.noreply.github.com>
2021-12-25 22:42:25 +0200

    Update suggest.js

--- Commands/Developer/suggest.js
+++ Commands/Developer/suggest.js
@@ -1,7 +1,25 @@
-const { CommandInteraction, MessageEmbed } = require('discord.js');
+const { CommandInteraction, MessageEmbed, Client} = require('discord.js');
 
 module.exports = {
     name: 'suggest',
     description: 'suggets things for bot',
-    async execute()
+    /**
+     * 
+     * @param {CommandInteraction} interaction 
+     * @param {Client} client 
+     */
+    async execute(interaction, client) {
+        const channel = interaction.guild.channels.cache.get('914552680339349514')
+
+        const Embed = new MessageEmbed()
+        .setTitle('Suggestion Submission')
+        .setDescription(`Hey in order to submit your suggestion simply type it below and it will be send automatically on <#914552680339349514> `)
+        .setColor('GREEN')
+        .setFooter('🛡️ Guardian Support')
+        .setTimestamp()
+
+        interaction.reply({content: 'Done now check dms to continue', ephemeral: true})
+
+        interaction.user.send({embeds: [Embed]})
+    }
 }
