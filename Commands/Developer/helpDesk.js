a74ad13c5e7c4ea5ca04a613ed1a783f5338eb0e
Nickolas001 <85787978+Nickolas001@users.noreply.github.com>
2022-01-05 15:38:59 +0200

    Update helpDesk.js

--- Commands/Developer/helpDesk.js
+++ Commands/Developer/helpDesk.js
@@ -120,7 +120,7 @@ module.exports = {
                 await b.reply({embeds: [new MessageEmbed().setDescription(`Guardian uses only channels ids and roles id. **No** personal data is stored such as passwords. Moreover you data is stored on [MongoDb](https://mongodb.com/) and can be accessed only by <@&914554015415697418> and <@791379233229504543>. Lastly your data is stored until the bot is out of the server `).setColor('GREEN').setFooter('🛡️ Guardian Support')], ephemeral: true}) } else if(b.customId === '5') {
                 
             } else if (b.customId === '5') {
-                await b.reply({embeds: [new MessageEmbed().setDescription(`You can purchuase premium [here](https://ko-fi.com/guardianbott/tiers) and after you buy the prefered tier you should receive <@&925748819986120764> and after you get the role open a ticket on <#925402791592202321> in order to reedem it`).setColor('GREEN').setTimestamp().setFooter('🛡️ Guardian Support')], ephemeral: true})
+                await b.reply({embeds: [new MessageEmbed().setDescription(`You can buy premium [here](https://ko-fi.com/guardianbott/tiers) and after the payment is successfull you should get the <@&925748819986120764> role, after you get this role open a ticket on <#925402791592202321> and wait for owner to come help you`).setColor('GREEN')], ephemeral: true})
             }
               }) 
             } 
